016519f51acedf799a3593d15776eb8ef3c02a61
DominatorVbN <voilentbynature@gmail.com>
2025-04-15 00:00:56 +0800

    Initial Commit

---
+++ Chapter/ChapterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChapterApp.swift
+//  Chapter
+//
+//  Created by Amit Samant on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ChapterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChapterTests/ChapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChapterTests.swift
+//  ChapterTests
+//
+//  Created by Amit Samant on 15/04/25.
+//
+
+import Testing
+@testable import Chapter
+
+struct ChapterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
